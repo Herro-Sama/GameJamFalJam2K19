19273b6c070220245bf09bee3b370abf95ae21a1
james <james@collins-house.com>
2019-01-25 20:39:44 +0000

    Adding base 3rd person project

---
+++ MakeIt/Source/MakeIt/MakeItGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MakeItGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AMakeItGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AMakeItGameMode();
+};
+
+
+
